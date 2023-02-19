e8390ac968c6e1326c58ec5cb082f929ab5b9858
Evgeny-Vorobyev <vorobyev.ev@gmail.com>
2023-02-19 15:49:17 +0300

    Прописал программу и добавил файл Program в репозиторий

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ShortStringArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] originalArray = new string[] { "London", "2023", "Moscow", "NY" };
+            string[] shortArray = new string[originalArray.Length];
+            int index = 0;
+
+            for (int i = 0; i < originalArray.Length; i++)
+            {
+                if (originalArray[i].Length <= 3)
+                {
+                    shortArray[index] = originalArray[i];
+                    index++;
+                }
+            }
+
+            Array.Resize(ref shortArray, index);
+
+            Console.WriteLine("Original array: " + string.Join(", ", originalArray));
+            Console.WriteLine("Short array: " + string.Join(", ", shortArray));
+        }
+    }
+}
